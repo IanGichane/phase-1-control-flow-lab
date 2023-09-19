65a1967244cb647e558ce04de18e36457e1f4137
Ian <gichaneian@gmail.com>
2023-09-19 11:35:34 +0300

    Completed workflow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  if(ride<=400){
+    return 'This one is on me!'
+  }else if(ride>=2000 && ride<=2500){
+    return 'I will gladly take your thirty bucks.'
+  }else if(ride>2500){
+    return 'No can do.'
+  }
 }
-
 function ternaryCheckCity(){
-  // Write your code here!
+  
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+return city === 'NYC'?'Ok, sounds good.':'No go.'
+
+}
+ternaryCheckCity('nai')
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  if(tip==='generous'){
+    return "Thank you so much."
+  }else if(tip=='not as generous'){
+    return "Thank you."
+  }else{
+    return "Bye." 
+  }
 }
